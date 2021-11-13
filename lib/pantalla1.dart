b4413d8f164d83160453364d0811bd13b52a4294
Discapaz <ignoranteambulante@gmail.com>
2021-11-13 15:55:19 -0500

    creacion pantalla1  y pantalla4

--- lib/pantalla1.dart
+++ lib/pantalla1.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:mivecindad/pantalla1.dart';
 import 'package:mivecindad/pantalla2.dart';
 import 'package:mivecindad/pantalla3.dart';
 
+/*
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp().then((value){
     runApp(const MyApp());
   });
 }
-
+*/
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mi vecindad',
+      title: 'Listado de negocios',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -38,53 +38,53 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-List datos_negocios=[];
+  List datos_negocios=[];
   void initState(){
     super.initState();
     getNegocios();
   }
 
-void getNegocios() async{
-  CollectionReference datos= FirebaseFirestore.instance.collection("Negocios"); //conecte a la coleccion
-  QuerySnapshot negocios= await datos.get(); //traer todos los negocios
-  if(negocios.docs.length>0){ //trae datos
-    print("Trae datos...");
-    for(var doc in negocios.docs){
-      print(doc.data());
-      setState(() {
-        datos_negocios.add(doc.data());
-      });
+  void getNegocios() async{
+    CollectionReference datos= FirebaseFirestore.instance.collection("Negocios"); //conecte a la coleccion
+    QuerySnapshot negocios= await datos.get(); //traer todos los negocios
+    if(negocios.docs.length>0){ //trae datos
+      print("Trae datos...");
+      for(var doc in negocios.docs){
+        print(doc.data());
+        setState(() {
+          datos_negocios.add(doc.data());
+        });
+      }
+    }else{
+      print("Ha fallado...");
     }
-  }else{
-    print("Ha fallado...");
   }
-}
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text("Mi vecindad"),
-      ),
-      body: Center(
-
-        child: ListView.builder(
-          itemCount: datos_negocios.length,
-          itemBuilder: (BuildContext context,i){
-            return ListTile(
-              title: Text("Negocio "+i.toString()+" - "+datos_negocios[i]['Nombre'].toString()),
-            );
-          },
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text("Mi vecindad"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
+        body: Center(
+
+          child: ListView.builder(
+            itemCount: datos_negocios.length,
+            itemBuilder: (BuildContext context,i){
+              return ListTile(
+                title: Text("Negocio "+i.toString()+" - "+datos_negocios[i]['Nombre'].toString()),
+              );
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
           onPressed: () {
             Navigator.push(context, MaterialPageRoute(builder: (context)=> pantalla2()));
           }, label: Text("Siguiente"),
-        icon: Icon(Icons.arrow_forward),
-      ),
+          icon: Icon(Icons.arrow_forward),
+        ),
         bottomNavigationBar: barrainferior()
 
     );
@@ -99,8 +99,8 @@ class barrainferior extends StatelessWidget {
       selectedItemColor: Colors.blue,
       unselectedItemColor: Colors.blue,
       onTap: (index){
-        if(index==0){
-         // print('hola');
+        if(index==1){
+          // print('hola');
           // Navigator.pop(context);}else if(index==1){
           //var t=DateTime.now();
           Navigator.push(context, MaterialPageRoute(builder: (context)=> pantalla2()));
@@ -108,12 +108,12 @@ class barrainferior extends StatelessWidget {
           //print(t);
         }else{
           print("va a consulta categoria");
-          Navigator.push(context, MaterialPageRoute(builder: (context)=> pantalla));
+          // Navigator.push(context, MaterialPageRoute(builder: (context)=> pantalla3(criterio)));
         }
 
       },
       items: [
-      /*  BottomNavigationBarItem(
+        /*  BottomNavigationBarItem(
             icon: Icon(Icons.arrow_back_outlined),
             label: 'Atras'
         ),*/
@@ -123,7 +123,7 @@ class barrainferior extends StatelessWidget {
         ),
         BottomNavigationBarItem(
             icon: Icon(Icons.cases),
-            label: 'Categorias'
+            label: 'categorias'
         )
       ],
     );
